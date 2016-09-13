e77e3aad04ab169d2c7181854262412b7c4d0039
mferris2421 <ferris.m@gmail.com>
2016-09-12 23:45:30 -0400

    Edited

--- firstProject/helloworld.py
+++ firstProject/helloworld.py
@@ -1,12 +1,17 @@
 # This program says hello and asks for my name.
 
 print('Hello world!')
-    print('What is your name?')     #ask the user for their name
+print('What is your name?')     #ask the user for their name
 
 myName = input()
 print('It is good to meet you, ' + myName)
 print('The length of your name is: ')
-    print(len(myName))
+print(len(myName))
 print('What is your age?')      #ask the user for their age
-    myAge = input()
-    print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
+print('I am ' + str(25) + " years old.")
+
+spam = input()
+spam = int(spam)
